76f2bf756193767df1948233794b5df9c2a828bd
SoorajSuresh <f.soorajsuresh@gmail.com>
2016-02-21 10:24:13 -0800

    Now with documentation.

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,3 +1,10 @@
+/**
+ * 
+ * This class politely greets the world.
+ * @author Sooraj Suresh
+ * @version 1.0.0
+ * 
+ */
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World, my name is Sooraj Suresh.\nNice to meet you!");
